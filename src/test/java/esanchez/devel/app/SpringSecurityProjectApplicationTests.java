1cd9617904cb70bb64dbb4e9c35b3fb77725f0df
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-05-18 15:22:23 +0200

    test different passwordEncoders for see the difference between them

--- src/test/java/esanchez/devel/app/SpringSecurityProjectApplicationTests.java
+++ src/test/java/esanchez/devel/app/SpringSecurityProjectApplicationTests.java
@@ -2,12 +2,18 @@ package esanchez.devel.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @SpringBootTest
 class SpringSecurityProjectApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testPasswordEncoders() {
+		System.out.println("Password: " + new BCryptPasswordEncoder().encode("password"));
+		System.out.println("Password: " + new Pbkdf2PasswordEncoder().encode("password"));
+		System.out.println("Password: " + new SCryptPasswordEncoder().encode("password"));
 	}
 
 }
