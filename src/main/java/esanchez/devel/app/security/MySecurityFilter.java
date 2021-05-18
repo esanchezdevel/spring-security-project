a13407d12abbcb3484a28a15de9b8ba6ff80e9ea
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-05-18 15:14:07 +0200

    more ways to create a custom filter

--- src/main/java/esanchez/devel/app/security/MySecurityFilter.java
+++ src/main/java/esanchez/devel/app/security/MySecurityFilter.java
@@ -8,6 +8,9 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 /**
  * 
  * @author Enrique Sanchez Jordan
@@ -16,8 +19,24 @@ import javax.servlet.ServletResponse;
  * It must be configured in the MySecurityConfig class
  *
  */
+
+/*
+ * Implements default Filter interface
+ */
 public class MySecurityFilter implements Filter {
 
+/*
+ * Extends GenericFilterBean is used when you want to initialize 
+ * a parameter in the filter and this class will generate the getters
+ * and setters automatically injecting the parameter
+ */
+//public class MySecurityFilter extends GenericFilterBean {
+	
+/*
+ * Extends OncePerRequestFilter for execute the filter only
+ * one time per request. The doFilter method is different in this case.
+ */
+//public class MySecurityFilter extends OncePerRequestFilter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
